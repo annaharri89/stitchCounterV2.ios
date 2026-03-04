fc5b2601f4336bcf516cfed458a2438a66f81e0c
Anna Harrison <16767360+annaharri89@users.noreply.github.com>
2026-03-03 20:37:24 -0500

    added the other themes the android app has

--- StitchCounter/Models/AppTheme.swift
+++ StitchCounter/Models/AppTheme.swift
@@ -2,16 +2,22 @@ import Foundation
 
 enum AppTheme: String, CaseIterable, Identifiable, Codable {
     case seaCottage = "sea_cottage"
-    case retroSummer = "retro_summer"
     case dustyRose = "dusty_rose"
+    case goldenHearth = "golden_hearth"
+    case forestFiber = "forest_fiber"
+    case cloudSoft = "cloud_soft"
+    case yarnCandy = "yarn_candy"
     
     var id: String { rawValue }
     
     var displayName: String {
         switch self {
         case .seaCottage: return "Sea Cottage"
-        case .retroSummer: return "Retro Summer"
         case .dustyRose: return "Dusty Rose"
+        case .goldenHearth: return "Golden Hearth"
+        case .forestFiber: return "Forest Fiber"
+        case .cloudSoft: return "Cloud Soft"
+        case .yarnCandy: return "Yarn Candy"
         }
     }
 }
